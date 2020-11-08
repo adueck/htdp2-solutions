7ccfb79f663201186a13ef2313eec367d16385c9
adueck <adueck@users.noreply.github.com>
2020-11-08 13:54:39 +0400

    finished III.16.4

--- 3-Abstraction/ex-263.rkt
+++ 3-Abstraction/ex-263.rkt
@@ -3,11 +3,15 @@
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-263) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; Nelon -> Number
 ; determines the smallest number on l
-(define (inf.v2 l)
+(define (inf l)
   (cond
     [(empty? (rest l)) (first l)]
     [else
-     (local ((define smallest-in-rest (inf.v2 (rest l))))
+     (local ((define smallest-in-rest (inf (rest l))))
        (if (< (first l) smallest-in-rest)
            (first l)
            smallest-in-rest))]))
+
+(inf (list 1 4 5))
+(inf (list 3 2))
+
