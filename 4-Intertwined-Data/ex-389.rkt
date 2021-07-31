d59041e73caf4cc8062e9d7733fb631a760ee81f
adueck <adueck@users.noreply.github.com>
2021-07-31 12:29:59 +0300

    finish IV.23.2

--- 4-Intertwined-Data/ex-389.rkt
+++ 4-Intertwined-Data/ex-389.rkt
@@ -5,4 +5,22 @@
 ; A PhoneRecord is a structure:
 ;   (make-phone-record String String)
 
-
+; Combines a list of names as strings and a list of phone numbers
+; as strings into a list of phone records
+; [List-of String] [List-of String] -> [List-of PhoneRecord]
+(check-expect (zip '() '()) '())
+(check-expect (zip '("Bill" "Frank" "Joe")
+                   '("123" "234" "345"))
+              `(,(make-phone-record "Bill" "123")
+                ,(make-phone-record "Frank" "234")
+                ,(make-phone-record "Joe" "345")))
+;(define (zip lon loph)
+;  (cond
+;    [(empty? lon) '()]
+;    [else (cons
+;            (make-phone-record (first lon) (first loph))
+;            (zip (rest lon) (rest loph)))]))
+(define (zip lon loph)
+  (map (lambda (n ph) (make-phone-record n ph))
+       lon
+       loph))
