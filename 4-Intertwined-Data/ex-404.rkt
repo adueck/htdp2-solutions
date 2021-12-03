c301a57d18c43240ec9d0a1f56ddf8c8568cf305
adueck <adueck@users.noreply.github.com>
2021-12-03 19:32:24 +0400

    starting IV.23.7

---
+++ 4-Intertwined-Data/ex-404.rkt
@@ -0,0 +1,26 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname ex-404) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+; [X X -> Boolean] [List-of X] [List-of X] -> Boolean
+; like andmap but for two lists of the same type and equal length
+(check-expect (andmap2 (lambda (x) (> x 5))
+                       '(6 9 10)
+                       '(50 87 90))
+              #true)
+(check-expect (andmap2 number?
+                       '(6 9 10)
+                       '(50 "c" 90))
+              #false)
+(check-expect (andmap2 string? '() '()) #true)
+; solution using abstraction
+(define (andmap2 f l1 l2)
+  (and
+   (andmap f l1)
+   (andmap f l2)))
+; solution from scratch using recursion
+; (define (andmap2 f l1 l2)
+;   (cond
+;     [(empty? l1) #true]
+;     [else (and (f (first l1))
+;                (f (first l2))
+;                (andmap2 f (rest l1) (rest l2)))]))
