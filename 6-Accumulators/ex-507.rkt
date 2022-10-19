8f8bcf5f81090f54ddb67726a6ecb536eea463de
adueck <adueck@users.noreply.github.com>
2022-10-19 17:16:33 +0500

    cleanup

--- 6-Accumulators/ex-507.rkt
+++ 6-Accumulators/ex-507.rkt
@@ -14,3 +14,18 @@
               [else
                (fold/a (f (first l) a) (rest l))])))
     (fold/a e l0)))
+
+; [X] Number [Number -> X] -> [List-of X]
+; Creates a list of n elements by applying proc
+; to the integers from 0 to (sub1 n) in order.
+(check-expect (build-l*st 3 add1) (build-list 3 add1))
+(check-expect (build-l*st 10 sub1) (build-list 10 sub1))
+(define (build-l*st n0 proc)
+  (local (; Number [List-of X]
+          ; accumulator a is a list of the numbers between
+          ; n0 and n that have been processed
+          (define (bl/a n a)
+            (cond
+              [(= n n0) (reverse a)]
+              [else (bl/a (add1 n) (cons (proc n) a))])))
+    (bl/a 0 '())))
